43c1aa5ce194ced36ca44ed0a469661b4e441bd6
Sten Uusvali <ztn@ztnmaps.com>
2020-11-21 06:24:07 -0800

    Microsteps back to 16. Increased extruder stepper current

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -749,7 +749,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 320, 320, 4031.5, 1886 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.1, 80.1, 1007.7, 471.5 }
 
 /**
  * Default Max Feed Rate (mm/s)
--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2287,7 +2287,7 @@
   #if AXIS_IS_TMC(X)
     #define X_CURRENT       955        // (mA) RMS current. Multiply by 1.414 for peak current.
     #define X_CURRENT_HOME  X_CURRENT  // (mA) RMS current for sensorless homing
-    #define X_MICROSTEPS     64        // 0..256
+    #define X_MICROSTEPS     16        // 0..256
     #define X_RSENSE          0.11
     #define X_CHAIN_POS      -1        // -1..0: Not chained. 1: MCU MOSI connected. 2: Next in chain, ...
     //#define X_INTERPOLATE  true      // Enable to override 'INTERPOLATE' for the X axis
@@ -2305,7 +2305,7 @@
   #if AXIS_IS_TMC(Y)
     #define Y_CURRENT       955
     #define Y_CURRENT_HOME  Y_CURRENT
-    #define Y_MICROSTEPS     64
+    #define Y_MICROSTEPS     16
     #define Y_RSENSE          0.11
     #define Y_CHAIN_POS      -1
     //#define Y_INTERPOLATE  true
@@ -2323,7 +2323,7 @@
   #if AXIS_IS_TMC(Z)
     #define Z_CURRENT       955
     #define Z_CURRENT_HOME  Z_CURRENT
-    #define Z_MICROSTEPS     64
+    #define Z_MICROSTEPS     16
     #define Z_RSENSE          0.11
     #define Z_CHAIN_POS      -1
     //#define Z_INTERPOLATE  true
@@ -2357,8 +2357,8 @@
   #endif
 
   #if AXIS_IS_TMC(E0)
-    #define E0_CURRENT      635
-    #define E0_MICROSTEPS    64
+    #define E0_CURRENT      700
+    #define E0_MICROSTEPS    16
     #define E0_RSENSE         0.11
     #define E0_CHAIN_POS     -1
     //#define E0_INTERPOLATE true
